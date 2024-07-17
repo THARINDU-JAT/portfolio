80942cef0d8642dde04fec187f5ff410cc07abb8
THARINDU-JAT <tharindu91.a@gmail.com>
2024-07-17 11:12:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,6 +62,6 @@ ScrollReveal({
  });
 
  ScrollReveal().reveal('.home-content, .heading, .heading-sub1', { origin:'top' });
- ScrollReveal().reveal('.home-img, .portfolio-box, .contact form, .skills-box, .certificates-box', { origin:'bottom' });
+ ScrollReveal().reveal('.home-img, .portfolio-box, .contact form, .skills-box, .certificates-box');
  ScrollReveal().reveal('.home-content h1, .about-img, .education-box, .title, .contact-box1', { origin:'left' });
  ScrollReveal().reveal('.home-content p, .about-content, .services-box, .heading-sub', { origin:'right' });
